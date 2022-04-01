1603c83d60acc11eb2a3697c01ddda86425350e6
yacine18 <hilali.yacin@gmail.com>
2022-04-01 18:16:12 +0000

    add-cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import router from "./routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", router);
 
